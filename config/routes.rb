5673d5f165fc2a5b321f6c65d77f99222c52cba2
raielin@gmail.com <raielin@gmail.com>
2014-10-15 17:25:19 -0400

    removed logout routes. was causing error: Invalid route name, already in use.

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
       omniauth_callbacks: "users/omniauth_callbacks"
     }
 
-  devise_scope :user do
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
